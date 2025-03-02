b5af769c06064b2785d3382a77f59d963dc7f03b
Samuel-Lucas <samuel.moreira10@outlook.com>
2025-03-01 22:01:03 -0300

    Creating endpoint for refresh token support

--- RestWithAspNet-PersonService/ApiPersonService/Controllers/AuthController.cs
+++ RestWithAspNet-PersonService/ApiPersonService/Controllers/AuthController.cs
@@ -21,11 +21,24 @@ public class AuthController : ControllerBase
     [Route("signin")]
     public IActionResult Signin([FromBody] UserVO user)
     {
-        if (user == null) return BadRequest("Invalid client request");
+        if (user is null) return BadRequest("Invalid client request");
 
         var token = _loginBusiness.ValidateCredentials(user);
 
-        if (token == null) return Unauthorized("User is not authorized to access");
+        if (token is null) return Unauthorized("User is not authorized to access");
+
+        return Ok(token);
+    }
+
+    [HttpPost]
+    [Route("refresh")]
+    public IActionResult Refresh([FromBody] TokenVO tokenVo)
+    {
+        if (tokenVo is null) return BadRequest("Invalid client request");
+
+        var token = _loginBusiness.ValidateCredentials(tokenVo);
+
+        if (token is null) return BadRequest("Invalid client request");
 
         return Ok(token);
     }
--- RestWithAspNet-PersonService/ApiPersonService/Data/VO/TokenVO.cs
+++ RestWithAspNet-PersonService/ApiPersonService/Data/VO/TokenVO.cs
@@ -2,7 +2,7 @@ namespace ApiPersonService.Data.VO;
 
 public class TokenVO
 {
-    public TokenVO(bool authenticated, string created, string expiration, string accessToken, string refreshToken)
+    public TokenVO(bool authenticated, string? created, string? expiration, string accessToken, string refreshToken)
     {
         Authenticated = authenticated;
         Created = created;
@@ -12,8 +12,8 @@ public class TokenVO
     }
 
     public bool Authenticated { get; set; }
-    public string Created { get; set; } = null!;
-    public string Expiration { get; set; } = null!;
+    public string? Created { get; set; }
+    public string? Expiration { get; set; }
     public string AccessToken { get; set; } = null!;
     public string RefreshToken { get; set; } = null!;
 }
