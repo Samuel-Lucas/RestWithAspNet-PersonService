bfb48fe233f063b200353deeaebc5d139ab04cd0
Samuel-Lucas <samuel.moreira10@outlook.com>
2025-07-05 11:14:20 -0300

    Configuring hateos enricher for PagedSearchVO

--- RestWithAspNet-PersonService/ApiPersonService/Hypermedia/ContentResponseEnricher.cs
+++ RestWithAspNet-PersonService/ApiPersonService/Hypermedia/ContentResponseEnricher.cs
@@ -1,5 +1,6 @@
 using System.Collections.Concurrent;
 using ApiPersonService.Hypermedia.Abstract;
+using ApiPersonService.Hypermedia.utils;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Mvc.Routing;
@@ -12,7 +13,7 @@ public abstract class ContentResponseEnricher<T> : IResponseEnricher where T : I
 
     public bool CanEnrich(Type contentType)
     {
-        return contentType == typeof(T) || contentType == typeof(List<T>);
+        return contentType == typeof(T) || contentType == typeof(List<T>) || contentType == typeof(PagedSearchVO<T>);
     }
 
     protected abstract Task EnrichModel(T content, IUrlHelper urlHelper);
@@ -40,6 +41,13 @@ public abstract class ContentResponseEnricher<T> : IResponseEnricher where T : I
             {
                 ConcurrentBag<T> bag = new ConcurrentBag<T>(collection);
                 Parallel.ForEach(bag, (element) =>
+                {
+                    EnrichModel(element, urlHelper);
+                });
+            }
+            else if (okObjectResult.Value is PagedSearchVO<T> pagedSearch)
+            {
+                Parallel.ForEach(pagedSearch.List.ToList(), (element) =>
                 {
                     EnrichModel(element, urlHelper);
                 });
