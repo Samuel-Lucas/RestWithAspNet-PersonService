83a30d3ccedb656bcbd67967e95df236730fac3a
Samuel-Lucas <samuel.moreira10@outlook.com>
2025-07-05 21:05:13 -0300

    Rollback commented authentication

--- RestWithAspNet-PersonService/ApiPersonService/Controllers/PersonController.cs
+++ RestWithAspNet-PersonService/ApiPersonService/Controllers/PersonController.cs
@@ -9,7 +9,7 @@ namespace ApiPersonService.Controllers;
 
 [ApiVersion("1")]
 [ApiController]
-// [Authorize("Bearer")]
+[Authorize("Bearer")]
 [Route("api/[controller]/v{version:apiVersion}")]
 public class PersonController : ControllerBase
 {
