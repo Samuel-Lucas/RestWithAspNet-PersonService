bcd504dbd011d9e6f2427d6e7d1022beb710375d
Samuel-Lucas <samuel.moreira10@outlook.com>
2025-07-06 16:10:35 -0300

    Developing method to save file

--- RestWithAspNet-PersonService/ApiPersonService/Business/Implementation/FileBusiness.cs
+++ RestWithAspNet-PersonService/ApiPersonService/Business/Implementation/FileBusiness.cs
@@ -17,14 +17,35 @@ public class FileBusiness : IFileBusiness
     {
         throw new NotImplementedException();
     }
+    
+    public async Task<FileDetailVO> SaveFileToDisk(IFormFile file)
+    {
+        var fileDetail = new FileDetailVO();
+        var fileType = Path.GetExtension(file.FileName);
+        var baseUrl = _context.HttpContext!.Request.Host;
+
+        if (fileType.ToLower() == "pdf" || fileType.ToLower() == ".jpg" ||
+            fileType.ToLower() == ".png" || fileType.ToLower() == ".jpeg")
+        {
+            var docName = Path.GetFileName(file.FileName);
+
+            if (file is not null && file.Length > 0)
+            {
+                var destination = Path.Combine(_basePath, "", docName);
+                fileDetail.DocumentName = docName;
+                fileDetail.DocType = fileType;
+                fileDetail.DocUrl = Path.Combine(baseUrl + "/api/file/v1/" + fileDetail.DocumentName);
+
+                using var stream = new FileStream(destination, FileMode.Create);
+                await file.CopyToAsync(stream);
+            }
+        }
+
+        return fileDetail;
+    }
 
     public async Task<List<FileDetailVO>> SaveFilesToDisk(IList<IFormFile> file)
     {
-        throw new NotImplementedException();
-    }
 
-    public async Task<FileDetailVO> SaveFileToDisk(IFormFile file)
-    {
-        throw new NotImplementedException();
     }
 }
