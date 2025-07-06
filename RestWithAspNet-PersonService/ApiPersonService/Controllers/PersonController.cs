7971fd9008a05b09a3f6c0b85ee9f67c970325e9
Samuel-Lucas <samuel.moreira10@outlook.com>
2025-07-05 21:04:13 -0300

    Implementing the controller method to execute paged search

--- RestWithAspNet-PersonService/ApiPersonService/Controllers/PersonController.cs
+++ RestWithAspNet-PersonService/ApiPersonService/Controllers/PersonController.cs
@@ -9,7 +9,7 @@ namespace ApiPersonService.Controllers;
 
 [ApiVersion("1")]
 [ApiController]
-[Authorize("Bearer")]
+// [Authorize("Bearer")]
 [Route("api/[controller]/v{version:apiVersion}")]
 public class PersonController : ControllerBase
 {
@@ -22,15 +22,20 @@ public class PersonController : ControllerBase
         _personService = personService;
     }
 
-    [HttpGet]
+    [HttpGet("{sortDirection}/{pageSize}/{page}")]
     [ProducesResponseType((200), Type = typeof(List<PersonVO>))]
     [ProducesResponseType((204))]
     [ProducesResponseType((400))]
     [ProducesResponseType((401))]
     [TypeFilter(typeof(HyperMediaFilter))]
-    public IActionResult Get()
+    public IActionResult Get(
+        [FromQuery] string? name,
+        string sortDirection,
+        int pageSize,
+        int page
+    )
     {
-        return Ok(_personService.FindAll());
+        return Ok(_personService.FindWithPagedSearch(name!, sortDirection, pageSize, page));
     }
 
     [HttpGet("{id}")]
